840dbbad721f4f9f50580ac3972b2fcf6d14d910
Derrick Wong <dcw008@ucsd.edu>
2017-02-24 12:05:17 -0800

    first commit

---
+++ Instagram-parse/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Instagram-parse
+//
+//  Created by Derrick Wong on 2/24/17.
+//  Copyright © 2017 Derrick Wong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
