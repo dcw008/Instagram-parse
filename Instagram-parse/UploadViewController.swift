402cc9ae3fe4ec4b3e994ae98b724598fe39f99e
Derrick Wong <dcw008@ucsd.edu>
2017-02-26 04:16:52 -0800

    added code for resizing

--- Instagram-parse/UploadViewController.swift
+++ Instagram-parse/UploadViewController.swift
@@ -10,10 +10,19 @@ import UIKit
 
 class UploadViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    //outlets
+    @IBOutlet weak var previewImage: UIImageView!
+    @IBOutlet weak var captionField: UITextView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //set the image to nil when the vc loads
+        self.previewImage = nil
+        
+        //set the text field to be empty when view loads
+        self.captionField.text = ""
 
     }
 
@@ -22,25 +31,24 @@ class UploadViewController: UIViewController, UIImagePickerControllerDelegate, U
         // Dispose of any resources that can be recreated.
     }
     
+    //allows user to select an image for upload
     func imagePickerController(_ picker: UIImagePickerController,
                                didFinishPickingMediaWithInfo info: [String : Any]) {
         // Get the image captured by the UIImagePickerController
         let originalImage = info[UIImagePickerControllerOriginalImage] as! UIImage
-        let editedImage = info[UIImagePickerControllerEditedImage] as! UIImage
         
-        
-        
-        // Do something with the images (based on your use case)
+        self.previewImage.image = originalImage
         
         // Dismiss UIImagePickerController to go back to your original view controller
         dismiss(animated: true, completion: nil)
     }
     
+    //when user presses cancel
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         dismiss(animated: true, completion: nil)
     }
     
-    
+    //when the user clicks on 'Library' button
     @IBAction func onUploadButton(_ sender: Any) {
         //user will be prompted to choose a photo
         let vc = UIImagePickerController()
@@ -51,15 +59,27 @@ class UploadViewController: UIViewController, UIImagePickerControllerDelegate, U
         self.present(vc, animated: true, completion: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    //sets the caption to write to Parse server
+    func setCaption(){
+        let caption = captionField.text
     }
-    */
+    
+    
+    //resize the image
+    func resize(image: UIImage, newSize: CGSize) -> UIImage {
+        
+        let resizeImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height))
+        
+        resizeImageView.contentMode = UIViewContentMode.scaleAspectFill
+        resizeImageView.image = image
+        
+        UIGraphicsBeginImageContext(resizeImageView.frame.size)
+        resizeImageView.layer.render(in: UIGraphicsGetCurrentContext()!)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return newImage!
+    }
+
 
 }
